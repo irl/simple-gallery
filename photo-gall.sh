b7774ee86db942d392444a93a20ceb75ca9cb213
yakamok <yakamok@users.noreply.github.com>
2016-03-14 00:20:42 +0100

    added name variable for watermark

--- photo-gall.sh
+++ photo-gall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #shell script to process and upload photos
+YOURNAME="add your name here for copyright watermark or remove this text"
 DIRLISTING=($(find *.jpg))
 count=$(cat count.txt)
 i=0
@@ -8,7 +9,7 @@ do
 	cp ${DIRLISTING[$i]} "backup/"$count".jpg"
 	mv ${DIRLISTING[$i]} $count".jpg"
 	mogrify -auto-orient -resize 1200 $count".jpg"
-	convert $count".jpg" -font Palatino -pointsize 13 -draw "gravity SouthEast fill black text 0,1 'copyright©yourname 2016' fill white text 0,2 'copyright©yourname 2016'" $count".jpg"
+	convert $count".jpg" -font Palatino -pointsize 13 -draw "gravity SouthEast fill black text 0,1 'copyright©$YOURNAME' fill white text 0,2 'copyright©$YOURNAME'" $count".jpg"
 	#add folder called old
 	mv $count".jpg" "old/"
 i=$(($i + 1))
