a02d4346002dd5a1e658cac3741722b05d5d9f36
yakamok <yakamok@users.noreply.github.com>
2016-03-14 00:12:37 +0100

    added watermarking to images

--- photo-gall.sh
+++ photo-gall.sh
@@ -8,6 +8,7 @@ do
 	cp ${DIRLISTING[$i]} "backup/"$count".jpg"
 	mv ${DIRLISTING[$i]} $count".jpg"
 	mogrify -auto-orient -resize 1200 $count".jpg"
+	convert $count".jpg" -font Palatino -pointsize 13 -draw "gravity SouthEast fill black text 0,1 'copyright©yourname 2016' fill white text 0,2 'copyright©yourname 2016'" $count".jpg"
 	#add folder called old
 	mv $count".jpg" "old/"
 i=$(($i + 1))
