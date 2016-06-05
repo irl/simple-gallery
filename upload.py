b86a43de600db09226e360312cab7c0c3280bb8c
yakamok <yakamok@users.noreply.github.com>
2016-06-05 02:47:52 +0100

    python version

---
+++ upload.py
@@ -0,0 +1,49 @@
+#send photos to gallery server
+
+import glob
+import os
+
+#------------------------------------------------------------------------
+maindir = "/home/user/photos-upload/" #where the photos and count file will be, use absolute paths
+offsiteloc = "url.org:/var/www/photos/" #the server to send to include exact path "myserver.com:/var/www/photos/"
+#------------------------------------------------------------------------
+
+if len(maindir) != 0:
+	if maindir[-1] != "/":
+		maindir = maindir + "/"
+
+if len(offsiteloc) != 0:
+	if offsiteloc[-1] != "/":
+		offsiteloc = offsiteloc + "/"
+
+if os.path.exists(maindir + "count.txt") == False:
+	with open(maindir + "count.txt","w") as handle:
+		handle.write("0")
+
+if os.path.exists(maindir + "old/") == False:
+	os.system("mkdir " + maindir + "old/")
+
+with open(maindir + "count.txt","r") as handle:
+	count = int(handle.read())
+
+files = glob.glob(maindir + "*")
+
+photos = []
+for x in files:
+	if ".jpg" in x or ".JPG" in x:
+		if x[-4:] == ".jpg" or x[-4:] == ".JPG":
+			photos.append(x)
+
+if len(photos) == 0:
+	print "Could not find any photos"
+else:
+	for x in photos:
+		os.system("mogrify -resize 640 " + x)
+		os.system("mv " + x + " " + maindir + str(count) + ".jpg")
+		count += 1
+
+	with open(maindir + "count.txt","w") as handle:
+		handle.write(str(count))
+	os.system("rsync -ave ssh " + maindir + "*.jpg " + offsiteloc)
+	os.system("mv " + maindir + "*.jpg " + maindir + "old/")
+	print "Finished Uploading images."
